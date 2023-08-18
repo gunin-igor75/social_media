1f31fde71249fe1830a81e87a25c90c3d5342308
Igor Gunin <gunin-igor75@yandex.ru>
2023-08-18 15:41:39 +0600

    first commit

---
+++ src/test/java/com/github/guninigor75/social_media/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guninigor75.social_media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
